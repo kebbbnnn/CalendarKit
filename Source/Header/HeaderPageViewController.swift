ad4fef514e2d8e4648b1bc7f7a562de9f0f08d54
Kevin Ladan <kevinladan@outlook.com>
2019-09-05 14:33:04 +0800

    Scroll end detector

--- Source/Header/HeaderPageViewController.swift
+++ Source/Header/HeaderPageViewController.swift
@@ -9,27 +9,32 @@ import UIKit
 
 public class HeaderPageViewController: UIPageViewController, UIScrollViewDelegate {
   public var scrollDelegate: UIScrollViewDelegate?
+  
+  public var isScrolling: Bool = false
 
   public override func viewDidLoad() {
     super.viewDidLoad()
-
     let scrollView = getScrollView()
-    scrollView?.delegate = scrollDelegate
+    scrollView.delegate = self
+  }
+  
+  public func getScrollView() -> UIScrollView {
+    return view.subviews.filter { $0 is UIScrollView }.first as! UIScrollView
   }
 
-  public func getScrollView() -> UIScrollView? {
-    var scrollView: UIScrollView?
-    for subview in view.subviews {
-      if let foundScrollView = subview as? UIScrollView {
-        scrollView = foundScrollView
-      }
-    }
-    return scrollView
+  public func scrollViewDidScroll(_ sender: UIScrollView) {
+    self.isScrolling = true
+    
+    NSObject.cancelPreviousPerformRequests(withTarget: self)
+    // ensure that the end of scroll is fired.
+    perform(#selector(UIScrollViewDelegate.scrollViewDidEndScrollingAnimation(_:)), with: sender, afterDelay: 0.2)
+    
+    self.scrollDelegate?.scrollViewDidScroll?(sender)
   }
-
-  public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    if let scrollDelegate = self.scrollDelegate {
-      scrollDelegate.scrollViewDidScroll?(scrollView)
-    }
+  
+  @objc public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+    NSObject.cancelPreviousPerformRequests(withTarget: self)
+    
+    self.isScrolling = false
   }
 }
